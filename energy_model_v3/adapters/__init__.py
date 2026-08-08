2eaf6a457f0d0260f792af54dc974e5216ceb880
januszjgal <januszgal@pm.me>
2026-08-08 18:13:48 -0400

    Parse locked market price products

--- energy_model_v3/adapters/__init__.py
+++ energy_model_v3/adapters/__init__.py
@@ -1,5 +1,24 @@
 """Locked-source market adapters."""
 
 from .markets import ADAPTERS, get_adapter
+from .parsers import (
+    parse_caiso_da,
+    parse_ercot_da,
+    parse_miso_da,
+    parse_nyiso_da,
+    parse_pjm_da,
+    parse_spp_da,
+    read_ercot_xlsx_sheet,
+)
 
-__all__ = ["ADAPTERS", "get_adapter"]
+__all__ = [
+    "ADAPTERS",
+    "get_adapter",
+    "parse_caiso_da",
+    "parse_ercot_da",
+    "parse_miso_da",
+    "parse_nyiso_da",
+    "parse_pjm_da",
+    "parse_spp_da",
+    "read_ercot_xlsx_sheet",
+]
